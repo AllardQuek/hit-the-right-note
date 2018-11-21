707335e37a2d98ce29660be344c0c213eceeeac0
Glitch (hello-webpage) <none>
2018-11-21 02:03:13 +0000

    ⛴👡 Checkpoint ./script.js:293015/4610 ./index.html:293015/197 ./style.css:293015/1423

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const NUM_BUTTONS = 8;
 const NOTES_PER_OCTAVE = 7;
 const OCTAVES = 7;
 
-const piano = {
+const config = {
   whiteNoteWidth: 20,
   blackNoteWidth: 20,
   whiteNoteHeight: 70,
   blackNoteHeight: 2 * 70 / 3
 }
 
-const p5Canvas = new p5(sketch, 'container');
+windowResized();
 
+
+window.addEventListener('resize', windowResized);
 document.addEventListener('keydown', (event) => {
   const button = event.keyCode - 49;
   if (button < 0 || button >= NUM_BUTTONS) {
@@ -31,64 +33,45 @@ document.addEventListener('keyup', (event) => {
 });
 
 function windowResized() {
-
+  const totalWhiteNotes = NOTES_PER_OCTAVE * OCTAVES;
+  config.whiteNoteWidth = window.innerWidth / totalWhiteNotes;
+  config.blackNoteWidth = config.whiteNoteWidth * 2 / 3;
+  svg.setAttribute('width', window.innerWidth);
+  svg.setAttribute('height', config.whiteNoteHeight);
+  
+  drawPiano();
 }
 function drawPiano() {
-
-}
-
-/****************
- * p5 canvas, draws:
- * - a pianoroll.
- ****************/
-
-function sketch(p) {
-  let whiteNoteWidth;
-  let blackNoteWidth;
-  let whiteNoteHeight = 70;
-  let blackNoteHeight = 2 * whiteNoteHeight / 3;
-  
-  p.setup = function() {
-    p.windowResized();
-  };
-  
-  p.windowResized = function () {
-    const totalWhiteNotes = NOTES_PER_OCTAVE * OCTAVES;
-
-    whiteNoteWidth = p.windowWidth / totalWhiteNotes;
-    blackNoteWidth = whiteNoteWidth * 2 / 3;
-    
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    drawPiano();
-  }
-  
-  p.update = function(button) {
-    // Pick a note at random for now.
-    const note = Math.floor(Math.random() * NOTES_PER_OCTAVE * OCTAVES);
-  }
-  
-  function drawPiano() {
-    p.clear();
-    let x = 0;
-    let y = p.height - whiteNoteHeight - 20;
-    const halfABlackNote = blackNoteWidth / 2;
-    
-    for (let o = 0; o < OCTAVES; o++) {
-      for (let i = 0; i < NOTES_PER_OCTAVE; i++) {
-        p.strokeWeight(3);
-        p.stroke('#141E30');
-        p.fill('white');
-        p.rect(x, y, whiteNoteWidth, whiteNoteHeight);
-
-        // No black notes for 0, 3.
-        if (i % NOTES_PER_OCTAVE !== 0 && i % NOTES_PER_OCTAVE !== 3) {
-          p.fill('black');
-          p.noStroke();
-          p.rect(x - halfABlackNote, y, blackNoteWidth, blackNoteHeight);
-        }
-        x += whiteNoteWidth;
+  let x = 0;
+  let y = 0; //p.height - whiteNoteHeight - 20;
+  const halfABlackNote = config.blackNoteWidth / 2;
+  const index = 0;
+  for (let o = 0; o < OCTAVES; o++) {
+    for (let i = 0; i < NOTES_PER_OCTAVE; i++) {
+      makeRect(i, x, y, config.whiteNoteWidth, config.whiteNoteHeight, 'white', '#141E30');
+      
+      // No black notes for 0, 3.
+      if (i % NOTES_PER_OCTAVE !== 0 && i % NOTES_PER_OCTAVE !== 3) {
+        makeRect(x - halfABlackNote, y, config.blackNoteWidth, config.blackNoteHeight, 'black');
       }
+      x += config.whiteNoteWidth;
     }
   }
 }
 
+function makeRect(x, y, w, h, fill, stroke) {
+  const svgNS = 'http://www.w3.org/2000/svg';
+  
+  const rect = document.createElementNS(svgNS, 'rect');
+  rect.setAttribute('x', x);
+  rect.setAttribute('y', y);
+  rect.setAttribute('width', w);
+  rect.setAttribute('height', h);
+  rect.setAttribute('fill', fill);
+  if (stroke) {
+    rect.setAttribute('stroke', stroke);
+    rect.setAttribute('stroke-width', '3px');
+  }
+  svg.appendChild(rect);
+  return rect;
+}
