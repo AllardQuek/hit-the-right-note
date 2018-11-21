c23b62bde79e00232a4f72e5c0d339394ace4a37
Glitch (hello-webpage) <none>
2018-11-21 01:19:06 +0000

    🐀🎰 Checkpoint ./style.css:293015/282 ./script.js:293015/497

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const p5Canvas = new p5(sketch, 'container');
 
+
+/****************
+ * p5 canvas, draws:
+ * - a pianoroll.
+ ****************/
+
 function sketch(p) {
   const NOTES_PER_OCTAVE = 7;
   const OCTAVES = 7;
   
   let whiteNoteWidth;
   let blackNoteWidth;
-  let whiteNoteHeight = 100;
+  let whiteNoteHeight = 70;
   let blackNoteHeight = 2 * whiteNoteHeight / 3;
   
   p.setup = function() {
-    console.log('setup');
     p.windowResized();
   };
   
   p.windowResized = function () {
-    console.log('resized');
-    
     const totalWhiteNotes = NOTES_PER_OCTAVE * OCTAVES;
 
-    whiteNoteWidth = Math.floor(p.windowWidth / totalWhiteNotes);
-    blackNoteWidth = Math.floor(whiteNoteWidth * 2 / 3);
+    whiteNoteWidth = p.windowWidth / totalWhiteNotes;
+    blackNoteWidth = whiteNoteWidth * 2 / 3;
     
     p.createCanvas(p.windowWidth, p.windowHeight);
     drawPiano();
@@ -28,16 +31,17 @@ function sketch(p) {
   
   function drawPiano() {
     let x = 0;
-    let y = p.height - whiteNoteHeight;
+    let y = p.height - whiteNoteHeight - 20;
     const halfABlackNote = blackNoteWidth / 2;
     
     for (let o = 0; o < OCTAVES; o++) {
       for (let i = 0; i < NOTES_PER_OCTAVE; i++) {
-        p.stroke('black');
+        p.strokeWeight(3);
+        p.stroke('#141E30');
         p.fill('white');
         p.rect(x, y, whiteNoteWidth, whiteNoteHeight);
 
-        // No black notes for 0, 3
+        // No black notes for 0, 3.
         if (i % NOTES_PER_OCTAVE !== 0 && i % NOTES_PER_OCTAVE !== 3) {
           p.fill('black');
           p.noStroke();
